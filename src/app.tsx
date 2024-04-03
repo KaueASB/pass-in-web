f9c4f43cc11b96e663de8a0e809506a40fef09ba
Kaue Alves <kaue_alves00@yahoo.com>
2024-04-03 16:30:31 -0300

    feat: Component Attendee List - create

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
+import { AttendeeList } from './components/attendee-list'
 import { Header } from './components/header'
 
 export function App() {
   return (
-    <div className="mx-auto flex max-w-[1216px] flex-col gap-5 py-5">
+    <div className="mx-auto flex max-w-[80%] flex-col gap-5 py-5">
       <Header />
+      <AttendeeList />
     </div>
   )
 }
